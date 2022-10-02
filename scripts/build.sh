24de5159bd358e15df396bde81a3b0a8b92b5d5d
Buddy Jonte <buddy@make-better.de>
2022-10-02 14:44:17 +0200

    Allow multiple tags in builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,13 @@
 
 set -eux
 
-docker pull $NAMESPACE:$IMAGE_VERSION || true
-docker build --compress --cache-from $NAMESPACE:$IMAGE_VERSION -t $NAMESPACE:$IMAGE_VERSION -t $NAMESPACE:latest -f $DOCKERFILE .
-docker push $NAMESPACE:$IMAGE_VERSION
+MAIN=$(echo ${TAGS} | cut -d' ' -f1)
+LIST=""
+
+for TAG in $TAGS; do
+  LIST="${LIST} -t ${NAMESPACE}:${TAG}"
+done
+
+docker pull $NAMESPACE:$MAIN || true
+docker build --compress --cache-from $NAMESPACE:$MAIN $LIST -f $DOCKERFILE .
+docker push $NAMESPACE:$MAIN
