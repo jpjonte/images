c4af29e44ed02254fdd77fcc4d515853f57d1d70
Buddy Jonte <buddy@make-better.de>
2024-04-22 09:33:05 +0200

    Add PHP 8.3, remove PHP 8.1

--- php/scripts/packages.sh
+++ php/scripts/packages.sh
@@ -6,12 +6,3 @@ apk --update --no-cache add \
     curl \
     acl \
     bash
-#    git \
-#    g++ \
-#    gcc \
-#    libc-dev \
-#    make \
-#    openssl \
-#    sudo \
-#    autoconf \
-#    openssl-dev \
