1c09e19dcadb1c0905f0f27026e9b15f1b8440b9
Buddy Jonte <buddy@make-better.de>
2026-02-13 09:23:04 +0100

    Always build latest stable Flutter version

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,13 @@ for TAG in $TAGS; do
   TAG_LIST="${TAG_LIST} --destination ${NAMESPACE}:${TAG}"
 done
 
+CACHE_REPO="$NAMESPACE/cache"
+
 /kaniko/executor \
   --context . \
   --dockerfile $DOCKERFILE \
+  --snapshot-mode=redo \
+  --cache=true \
+  --cache-repo $CACHE_REPO \
+  ${BUILD_ARGS:-} \
   $TAG_LIST
